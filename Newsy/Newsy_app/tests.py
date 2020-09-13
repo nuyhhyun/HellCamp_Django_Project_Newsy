53af521d57ca1ce26ec2a3b8c7c8191f31a6a1c1
bunseokbot <bunseokbot@gmail.com>
2020-09-13 15:26:18 +0900

    Add setUp method in testcode

--- Newsy/Newsy_app/tests.py
+++ Newsy/Newsy_app/tests.py
@@ -2,83 +2,71 @@ from django.test import TestCase, Client
 import requests
 
 
-class TestCase1(TestCase):
+class NewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_is_the_requests_done(self):
         res = requests.get("http://rss.nocutnews.co.kr/Nocutnews.xml")
         self.assertEqual(res.status_code, 200)
 
     def test_view_category_news(self):
-        c = Client()
-        resp = c.get("/news/Latest")
+        resp = self.client.get("/news/Latest")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_politics(self):
-        c = Client()
-        resp = c.get("/Politics/Politics")
+        resp = self.client.get("/Politics/Politics")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_social(self):
-        c = Client()
-        resp = c.get("/Social/Social")
+        resp = self.client.get("/Social/Social")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_economy(self):
-        c = Client()
-        resp = c.get("/Economy/Economy")
+        resp = self.client.get("/Economy/Economy")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_sports(self):
-        c = Client()
-        resp = c.get("/Sports/Sports")
+        resp = self.client.get("/Sports/Sports")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_local(self):
-        c = Client()
-        resp = c.get("/Local/Local")
+        resp = self.client.get("/Local/Local")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_global(self):
-        c = Client()
-        resp = c.get("/Global/Global")
+        resp = self.client.get("/Global/Global")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_sports(self):
-        c = Client()
-        resp = c.get("/Sports/Sports")
+        resp = self.client.get("/Sports/Sports")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_it(self):
-        c = Client()
-        resp = c.get("/IT/IT")
+        resp = self.client.get("/IT/IT")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_enter(self):
-        c = Client()
-        resp = c.get("/Enter/Entertain")
+        resp = self.client.get("/Enter/Entertain")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_culture(self):
-        c = Client()
-        resp = c.get("/Culture/Culture")
+        resp = self.client.get("/Culture/Culture")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_column(self):
-        c = Client()
-        resp = c.get("/Column/Column")
+        resp = self.client.get("/Column/Column")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_photo(self):
-        c = Client()
-        resp = c.get("/Photo/Photo")
+        resp = self.client.get("/Photo/Photo")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_scoop(self):
-        c = Client()
-        resp = c.get("/Only/Scoop")
+        resp = self.client.get("/Only/Scoop")
         self.assertEqual(resp.status_code, 200)
 
     def test_view_category_home(self):
-        c = Client()
-        resp = c.get("/")
+        resp = self.client.get("/")
         self.assertEqual(resp.status_code, 200)
 
